439492f7533c2f13a27b005f57667a6334a1c3c7
yunhao <851549562@qq.com>
2018-03-15 16:08:10 +0000

    add function in yh.c

--- yh.c
+++ yh.c
@@ -7,38 +7,19 @@
 
 void yh_wait(){
     int num;
-    printf("enter 1:");
+    printf("enter 1 to continue, enter 2 to exit program:");
     scanf("%d", &num);
     if(num == 1)
-        printf("ok\n");
+        printf("continue\n");
+    else if(num == 2){
+        printf("exit\n");
+        exit(0);
+    }
+    else yh_wait();
 }
 
 void yh_print_s(u8* buf){
     printf("%s\n", buf);
-}
-
-void yh_write_file(u8* out_file, u8* buf, s32 len, s32 apped){
-    if(apped == 0){
-        s32 fd = open(out_file, O_RDWR | O_CREAT, 0666);
-        write(fd, buf, len);
-        close(fd);
-    }
-    else{
-        printf("append mode\n");
-        s32 fd = open(out_file, O_RDWR | O_CREAT | O_APPEND, 0666);
-        write(fd, buf, len);
-        close(fd);
-    }
-}
-
-void yh_read_file(u8* in_file, u8* buf, s32 len){
-    s32 fd = open(in_file, O_RDWR | O_CREAT | O_EXCL, 0666);
-    if(fd < 0){
-        close(fd);
-        fd = open(in_file, O_RDWR);
-        read(fd, buf, len);
-    }
-    close(fd);
 }
 
 void yh_check_ifdef(){
@@ -112,3 +93,260 @@ void yh_check_ifdef(){
     printf("__sun no\n");
 #endif
 }
+
+void yh_fprintf_str(u8* out_file, u8* mode, char* context){
+    FILE *out = fopen(out_file, mode);
+    if(out == NULL){
+        printf("create new one");
+        yh_wait();
+        out = fopen(out_file, "w");
+        fclose(out);
+        out = fopen(out_file, mode);
+    }
+    fprintf(out, context, NULL);
+    fclose(out);
+}
+
+void yh_mark_start(){
+    printf("yh mark start point\n");
+}
+
+void yh_mark_end(){
+    printf("yh mark end point\n");
+}
+
+void yh_check_bits_u64(u64 num){
+    int i = 64;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_u32(u32 num){
+    int i = 32;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_u16(u16 num){
+    int i = 16;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_u8(u8 num){
+    int i = 8;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+
+void yh_check_bits_s64(s64 num){
+    int i = 64;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_s32(s32 num){
+    int i = 32;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_s16(s16 num){
+    int i = 16;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+void yh_check_bits_s8(s8 num){
+    int i = 8;
+    int stk[100];
+    memset(stk, -1, sizeof(stk));
+    int cur = 0;
+    while(i--){
+        stk[cur++] = num & 1;
+        num >>= 1;
+    }
+    int cnt = 0;
+    while(cur){
+        printf("%d", stk[cur-1]);
+        cur--;
+        cnt++;
+        if(cnt == 8){
+            printf(" ");
+            cnt = 0;
+        }
+    }
+    printf("\n");
+}
+
+void yh_change_binary_to_ascii(u8* in_file, u8* out_file){
+    FILE* in = fopen(in_file, "rb");
+    FILE* out = fopen(out_file, "w");
+    char buf[10];
+    int actual_size = 0;
+    while(actual_size = fread(buf, 1, 8, in)){
+        int i = 0;
+        for (; i < actual_size; i++){
+            fprintf(out, "%c", buf[i]);
+        }
+    }
+    fclose(out);
+    fclose(in);
+}
+
+void yh_change_binary_to_bits(u8* in_file, u8* out_file){
+    FILE* in = fopen(in_file, "rb");
+    FILE* out = fopen(out_file, "w");
+    char buf[10];
+    int actual_size = 0;
+    while(actual_size = fread(buf, 1, 8, in)){
+        int i = 0;
+        for (; i < actual_size; i++){
+            int j = 8;
+            int stk[10];
+            int cur = 0;
+            while(j--){
+                stk[cur++] = buf[i] & 1;
+                buf[i] >>= 1;
+            }
+            while(cur){
+                fprintf(out, "%d", stk[cur - 1]);
+                cur--;
+            }
+            fprintf(out, " ");
+        }
+        fprintf(out, "\n");
+    }
+    fclose(in);
+    fclose(out);
+}
+
+//void yh_write_file(u8* out_file, u8* buf, s32 len, s32 apped){
+//    if(apped == 0){
+//        s32 fd = open(out_file, O_RDWR | O_CREAT, 0666);
+//        write(fd, buf, len);
+//        close(fd);
+//    }
+//    else{
+//        printf("append mode\n");
+//        s32 fd = open(out_file, O_RDWR | O_CREAT | O_APPEND, 0666);
+//        write(fd, buf, len);
+//        close(fd);
+//    }
+//}
+
+//void yh_read_file(u8* in_file, u8* buf, s32 len){
+//    s32 fd = open(in_file, O_RDWR | O_CREAT | O_EXCL, 0666);
+//    if(fd < 0){
+//        close(fd);
+//        fd = open(in_file, O_RDWR);
+//        read(fd, buf, len);
+//    }
+//    close(fd);
+//}
--- yh.h
+++ yh.h
@@ -6,17 +6,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <sys/file.h>
 #include <string.h>
 #include "types.h"
+#include <sys/file.h>
 
 void yh_wait();
 
 void yh_print_s(u8* buf);
 
-void yh_write_file(u8* out_file, u8* buf, s32 len, s32 apped);
-
-void yh_read_file(u8* in_file, u8* buf, s32 len);
-
 void yh_check_ifdef();
 
+void yh_fprintf_str(u8* out_file, u8* mode, char* context);
+
+void yh_mark_start();
+
+void yh_mark_end();
+
+void yh_check_bits_u64(u64 num);
+void yh_check_bits_u32(u32 num);
+void yh_check_bits_u16(u16 num);
+void yh_check_bits_u8(u8 num);
+void yh_check_bits_s64(s64 num);
+void yh_check_bits_s32(s32 num);
+void yh_check_bits_s16(s16 num);
+void yh_check_bits_s8(s8 num);
+
+void yh_change_binary_to_ascii(u8* in_file, u8* out_file);
+
+void yh_change_binary_to_bits(u8* in_file, u8* out_file);
+
+//void yh_write_file(u8* out_file, u8* buf, s32 len, s32 apped);
+
+//void yh_read_file(u8* in_file, u8* buf, s32 len);
